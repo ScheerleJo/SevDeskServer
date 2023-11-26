66a079470119da7f475cbe17d1cc23f37c9929dd
ScheerleJo <scheerle.josia@gmail.com>
2023-11-26 21:18:19 +0100

    Partially fix output. Still missing reading [7] of undefined at 69

--- output.js
+++ output.js
@@ -21,37 +21,52 @@ function setSortedData (data) {
  */
 function formatData (){
     let formattedData = [];
-    let lastCustomerNUmber = 0;
+    let lastCustomerNumber = 0;
     let donator = -1; // important to have at -1 to avoid the first element in the array to be empty. 
     
     for (let i = 0; i < sortedData.length; i++) {
         const element = sortedData[i];
         let customerNumber = sortedData[i].supplier.customerNumber || undefined;
-        if(customerNumber != lastCustomerNUmber && customerNumber != undefined){
-            lastCustomerNUmber = customerNumber;
+        if(customerNumber != lastCustomerNumber && customerNumber != undefined){
+            lastCustomerNumber = customerNumber;
             donator++;
             let adress;
-            request.getAdressByContactID(element.supplier.id, (resData) => {
-                adress = resData.objects;
+            request.getAdressByContactID(element.supplier.id, () => {
+                
+                adress = request.getAdressData().objects;
+                let nextDonator = [
+                    element.supplier.customerNumber,
+                    element.supplier.academicTitle,
+                    element.supplier.surename,
+                    element.supplier.familyname,
+                    adress.street,
+                    `${adress.zip} ${adress.city}`,
+                    formattedData[donator][6] += element.sumNet,
+                    []
+                ]
+
+                
+                formattedData.push(nextDonator)
             });
-            formattedData[donator] = [
-                element.supplier.customerNumber,
-                element.supplier.academicTitle,
-                element.supplier.surename,
-                element.supplier.familyname,
-                adress.street,
-                `${adress.zip} ${adress.city}`,
-                formattedData[donator][6] += element.sumNet,
-                []
-            ]
-        }    
+            
+        } else if (customerNumber == undefined) {
+            donator++;
+            let name = element.supplierName.split(" ");
+            let surname, familyname = "";
+            for (let j = 0; j < (name.length - 1); j++) {
+                surname += (name[i]);
+            } 
+            formattedData.push([
+                "Error, keine Kdnr registriert", "", surename, name[-1],"","", element.sumNet,[]
+            ]);
+        }
         let donation = [
             element.voucherDate,
             element.description,
             element.sumNet,
             convertNumToWord(element.sumNet)
         ]
-        formattedData[donator][7].push(donation)
+        formattedData[formatData.length -1][7].push(donation)              
     }
 
     return formattedData;
@@ -59,5 +74,5 @@ function formatData (){
 
 function convertNumToWord(numInteger) {
     //Uppercase: Default True
-    return num2words(numInteger)
+    return num2words.numToWord(numInteger)
 }
