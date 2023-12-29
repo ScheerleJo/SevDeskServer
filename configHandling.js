6b44087e9cfbf8b1af1c3db5936f2e06c3d1ce40
ScheerleJo <scheerle.josia@gmail.com>
2023-12-29 12:09:21 +0100

    correct ConfigHandler

--- configHandling.js
+++ configHandling.js
@@ -2,14 +2,15 @@ DocumentType = module;
 
 const config = require('config');
 
+const PORT = config.get('server.port');
+const VERSION = config.get('application.server-version');
+
 module.exports = {
     PORT,
     VERSION,
     getSaveDataPath
 }
 
-const PORT = config.get('server.port');
-const VERSION = config.get('application.version');
 
 function getSaveDataPath() {
     return config.get('activeData.path') + config.get("activeData.filename");
