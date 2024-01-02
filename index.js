994218e9bf55bbff49160d55be09e178087f10d0
ScheerleJo <scheerle.josia@gmail.com>
2024-01-02 11:15:48 +0100

    Backend Cleanup and URL-Parsing fixed

--- index.js
+++ index.js
@@ -1,38 +1,68 @@
-const express = require('express');
-var app = express();
-const PORT = require('./configHandling').PORT;
-const VERSION = require('./configHandling').VERSION;
+const config = require('./configHandling')
 const requests = require('./requests');
-const out = require('./output');
+const formatting = require('./formatting');
+const sort = require('./sorting');
 const fileHandler = require('./fileHandling');
+const urlHandler = require('./urlHandling');
+const process = require('node:process');
+const express = require('express');
+const cors = require('cors');
+var app = express();
 
 
+let corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200,
+    methods: "GET, PUT, POST"
+}
+
+// Options for the Debug Helper to function properly
+app.use(cors(corsOptions));
+
+
+
+const VERSION = config.VERSION;
+const PORT = config.PORT;
+
+let donationData = undefined;
 app.get('/', (req, res) => {
-    console.log("successful request");
-    // res.text("Successful");
-    res.send("Successful")
+    res.send({
+        "status": "running",
+        "message": "Invalid Branch. Use '/getDonations?year=...' to get all Donations to the input year Use '/saveData?data=...' to save the input Data to a json file"
+});
+});
+app.get('/kill', (req, res) => {
+    console.log('The Server will Shutdown with ExitCode 1');
+    res.send({"status":"shutdown"});
+    process.exit()
 });
 
 app.get('/getDonations', (req, res) => {
-    let year = requests.getYearFromQuery(req.url);
-    requests.getDonations(2023, () => {
+    requests.getDonations(urlHandler.getYearFromQuery(req.url), () => {
         console.log('DATA GATHERING (Donations) COMPLETE')
-        out.setDonationData(requests.getData());
-        requests.getAllAdresses(() => {
+        // console.log(requests.getData());
+        formatting.setDonationData(requests.getData());
+        requests.getAllAddresses(() => {
             console.log('DATA GATHERING (Addresses) COMPLETE')
-            out.setAddressData(requests.getData());
-            let response = out.newFormat();
-            // console.log(response);
-            
-            res.send(response);
+            formatting.setAddressData(requests.getAddressData());
+            donationData = formatting.newFormat();
+            res.send(donationData);
+            console.log('DATA PUSHED SUCCESSFULLY');
         })
     })    
 });
 app.get('/saveData', (req, res) => {
-    let response = fileHandler.saveStatusToFile(req.url);
+    let response = fileHandler.saveStatusToFile(donationData);
     res.send({"status": response});     //  Send Status Code (200 for everything okay)
+});
+
+app.get('/deleteItem', (req, res) => { ///deleteItem?donatorIndex=...(num)&donationIndex=...(num)&deleteAll=...(true/false)
+    sort.setDonationData(donationData);
+    
+    donationData = sort.deleteItemAtIndex(urlHandler.getDeleteItem(req.url));
 });
 
 app.listen(PORT, function(){  
     console.log(`SevDesk-Extension for BFU-Worms running on Version: ${VERSION}\nServer running on Port ${PORT}`);
 });
+
---
+++ urlHandling.js
@@ -0,0 +1,39 @@
+DocumentType = module;
+
+const parseUrl = require('url-parse');
+
+
+module.exports = {
+    getYearFromQuery,
+    getDeleteItem
+}
+
+
+/**
+ * Get Year from QueryParams
+ * @param {url} url req.url of express.get()
+ * @returns {Number} year
+ */
+function getYearFromQuery(url) {
+    return parseUrl(url, true).query.year;
+}
+
+
+/**
+ * Get QueryParams from '/deleteItem' request
+ * @param {url} url
+ * @returns {Array<String>} [DonatorIndex, DonationIndex, DeleteAll | false]
+ */
+function getDeleteItem(url) {
+    let deleteInfo = parseUrl(url, true).query;
+    if(deleteInfo.donationIndex == undefined) {
+        deleteInfo.donationIndex = false;
+    }
+    if(deleteInfo.deleteAll == undefined){
+        deleteInfo.deleteAll = false;
+    }
+    if(deleteInfo.donatorIndex == undefined) {
+        return console.error('Error at /deleteItem Request!\nNo donator was specified to delete.\n\n Error was thrown at `urlHandling.js:36`');
+    }
+    return deleteInfo;
+}
