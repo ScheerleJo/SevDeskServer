07076a37f96ec02954fcc4deb37df27e406e2b0d
Josia Scheerle <scheerle.josia@gmail.com>
2024-05-28 12:41:28 +0200

    outsourcing functions and proper async handling

---
+++ scripts/functionHandling.js
@@ -0,0 +1,41 @@
+module.exports = {
+    fetchNew,
+    refetchUsers
+}
+
+const requests = require('./requests');
+const formatting = require('./formatting');
+const urlHandler = require('./urlHandling');
+const sort = require('./sorting');
+
+async function fetchNew(req) {
+    year = urlHandler.getYear(req.url)
+    formatting.setDonationData(await requests.getDonations(year));
+    console.log('DATA GATHERING (Donations) COMPLETE')
+    formatting.setAddressData(await requests.getAllAddresses());
+    console.log('DATA GATHERING (Addresses) COMPLETE')
+    return {
+        "Year": year,
+        "Data": await formatting.newFormat()
+    }
+}
+
+async function refetchUsers(req, year, donationData) {
+    let users = urlHandler.getMultipleUserIDs(req.url);
+
+    formatting.setDonationData(await requests.getDonations(year, users.length == 1 ? users.ID[0] : undefined));
+    formatting.setAddressData(await requests.getAllAddresses());
+    let formattedData = formatting.newFormat();
+
+    //Indexes for new elements in old 'donationData'-Array
+    let indexes = sort.getIndexforUserID(donationData, userIDs);
+
+    for (let i = 0; i < formattedData.length; i++) {
+        if(indexes[i] == -1) {
+            console.error(`Error while updating users. UserID ${formattedData[i].ID}`);
+            continue;
+        }
+        donationData[indexes[i]] = formattedData[i];
+    }
+    return donationData;    
+}
--- scripts/requests.js
+++ scripts/requests.js
@@ -6,20 +6,26 @@ let addressData = 'Hello, i am Data'
 
 module.exports = {
     getDonations,
-    getUserSpecificDonations,
     getData,
     getAddressData,
     getAllAddresses
 }
 
 
-async function makeSevDeskRequestGET(querystring) {
+async function  makeSevDeskRequestGET(querystring) {
     require('dotenv').config();
     let token = process.env.API_TOKEN;
     baseUrl = 'https://my.sevdesk.de/api/v1/';
     let req = baseUrl + querystring;
     let config =  { headers:{'Authorization': token } }
-    let response;
+    try {
+        const response = await axios.get(req, config);
+        return response.data;
+    } catch (error) {
+        console.error(error);
+    }
+
+    /*
     response = await axios.get(req, config).catch((error) => {
         console.log(error);
     });    
@@ -30,21 +36,18 @@ async function makeSevDeskRequestGET(querystring) {
         await setTimeout(() => {return;}, 500);
         console.log('sleep');
     }
-    let resData = await response.data
-    return await resData
+    return await response.data;*/
 }
 
-
-async function getDonations(year, _callback){
+async function getDonations(year, user = undefined){
     let request = "Voucher?embed=accountingTypes%2CaccountingTypes.accountingSystemNumber%2Csupplier%2Csupplier.category%2Cobject%2Cdebit%2Cdelinquent&countAll=true&limit=none&voucherType=VOU&emptyState=true&accountingType[id]=679076&accountingType[objectName]=AccountingType&year=" + year
-    responseData = await makeSevDeskRequestGET(request);
-    _callback();
+    if(user != undefined) request += "&contact[objectName]=Contact&contact[id]=" + user;
+    return await makeSevDeskRequestGET(request);
 }
 
 async function getAllAddresses(_callback) {
     let request = "ContactAddress?limit=none&embed=contact%2Cstreet%2Czip%2Ccity%2Ccountry";
-    addressData = await makeSevDeskRequestGET(request);
-    _callback();
+    return await makeSevDeskRequestGET(request);
 }
 
 function getData(){
